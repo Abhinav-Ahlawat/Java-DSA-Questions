38ec952169467cd88bfdc96800cb4351e2b56386
Abhinav-Ahlawat <72443217+Abhinav-Ahlawat@users.noreply.github.com>
2024-07-04 09:47:55 +0530

    Add files via upload
    Java program to check largest element in an array.

---
+++ largestElementInArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class largestElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int largest=arr[0];
+		for(int i=1;i<n;i++) {
+			if(arr[i]>largest){
+				largest=arr[i];
+			}
+		}
+		System.out.println("Largest element is"+largest);
+	}
+
+}
